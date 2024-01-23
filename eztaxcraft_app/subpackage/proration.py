78408daac03670d031cc4b7f85d6fdaad60a332d
NatsukiShimizu <n.shimizu023@gmail.com>
2024-01-23 19:57:40 +0900

    引数の名称を修正

--- eztaxcraft_app/subpackage/proration.py
+++ eztaxcraft_app/subpackage/proration.py
@@ -4,13 +4,13 @@
 class Proration:
     """按分クラス
     """
-    def calc_expenses(self, source_value: float, target_value: float, cost: int) -> int:
+    def calc_expenses(self, source_value: float, target_value: float, _per_month: int) -> int:
         """経費按分計算
 
         Args:
             source_value (float): ベースの値
             target_value (float): 経費精算したい部分の値
-            cost (int): 1か月の経費
+            _per_month (int): 1か月の経費
 
         Returns:
             int: 必要経費額
@@ -20,6 +20,8 @@ class Proration:
         # 比率の表示
         print(f'按分用比率: {ratio:.2%}')
         # 必要経費計算
-        required_expenses = cost * ratio
+        required_expenses = _per_month * ratio
 
         return int(required_expenses)
+    
+
